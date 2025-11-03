390e05e003c618c922d68d010d3ced8eb8538e82
ktyagiapphelix2u <ktyagi-apphelix@2u.com>
2025-11-03 11:37:11 +0000

    chore: rmeoval of mktg link map

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -1208,7 +1208,6 @@ INSTALLED_APPS = [
 
 ################# EDX MARKETING SITE ##################################
 
-MKTG_URL_LINK_MAP = {}
 
 ID_VERIFICATION_SUPPORT_LINK = ''
 PASSWORD_RESET_SUPPORT_LINK = ''
